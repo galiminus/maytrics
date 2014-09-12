ccd69f4affa0fe693e84eb6a0e7183e2d0a65cf2
Victor Goya <goya.victor@gmail.com>
2014-09-12 20:45:27 +0800

    better logging

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@
 
 #include <jansson.h>
 
-struct maytrics_server {
+struct maytrics {
   redisContext *	      redis;
 
   regex_t *                   metrics_regex;
@@ -21,6 +21,7 @@ struct maytrics_server {
 };
 
 enum {
+  LOG_FATAL,
   LOG_ERROR,
   LOG_INFO,
   LOG_DEBUG
@@ -29,7 +30,11 @@ enum {
 int log_level = LOG_ERROR; /* error */
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
-#define log_err(M, ...) if (log_level >= LOG_ERROR) {								\
+#define log_fatal(M, ...) if (log_level >= LOG_FATAL) {								\
+    fprintf(stdout, "[FATAL] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__);	\
+}
+
+#define log_error(M, ...) if (log_level >= LOG_ERROR) {								\
     fprintf(stdout, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__);	\
 }
 
@@ -58,22 +63,27 @@ set_metrics_comment (evhtp_request_t *  req,
   if (comment) {
     evbuffer_add_printf (req->buffer_out, "{comment: \"%s\"}", comment);
   }
-  log_info ("%s: %d\n", req->uri->path->full, status);
+  log_debug ("%s: %d", req->uri->path->full, status);
 
   return ;
 }
 
 int
 send_value_from_url (evhtp_request_t *               req,
-                     struct maytrics_server *        maytrics_server)
+                     struct maytrics *        maytrics)
 {
   int			status;
   redisReply *		reply;
 
-  reply = redisCommand (maytrics_server->redis, "GET %b",
+  reply = redisCommand (maytrics->redis, "GET %b",
 			&req->uri->path->full[req->uri->path->matched_soff],
 			req->uri->path->matched_eoff - req->uri->path->matched_soff);
-  if (reply == NULL) {
+  if (reply == NULL || reply->type == REDIS_REPLY_ERROR) {
+    log_error ("redisCommand() failed: %s", maytrics->redis->errstr);
+    if (reply) {
+      freeReplyObject (reply);
+    }
+
     status = EVHTP_RES_SERVERR;
     goto exit;
   }
@@ -95,21 +105,21 @@ send_value_from_url (evhtp_request_t *               req,
 
 int
 metrics_controller_get (evhtp_request_t *               req,
-                        struct maytrics_server *        maytrics_server)
+                        struct maytrics *        maytrics)
 {
-  return (send_value_from_url (req, maytrics_server));
+  return (send_value_from_url (req, maytrics));
 }
 
 int
 metric_controller_get (evhtp_request_t *               req,
-                       struct maytrics_server *        maytrics_server)
+                       struct maytrics *        maytrics)
 {
-  return (send_value_from_url (req, maytrics_server));
+  return (send_value_from_url (req, maytrics));
 }
 
 int
 metric_controller_post (evhtp_request_t *               req,
-                        struct maytrics_server *        maytrics_server)
+                        struct maytrics *        maytrics)
 {
 #define METRICS_REGEX_MAX_MATCH 3
 
@@ -135,15 +145,17 @@ metric_controller_post (evhtp_request_t *               req,
 
   redisReply *	      reply;
 
-  if (regexec (maytrics_server->metrics_regex,
+  if (regexec (maytrics->metrics_regex,
 	       req->uri->path->full,
 	       METRICS_REGEX_MAX_MATCH + 1, pmatch, 0) == REG_NOMATCH) {
+    log_error ("regexec() failed.")
     status = EVHTP_RES_SERVERR;
     goto error;
   }
   user = strndup (&req->uri->path->full[pmatch[1].rm_so],
 		  pmatch[1].rm_eo - pmatch[1].rm_so);
   if (user == NULL) {
+    log_error ("strndup() failed.")
     status = EVHTP_RES_SERVERR;
     goto error;
   }
@@ -151,6 +163,7 @@ metric_controller_post (evhtp_request_t *               req,
   metric = strndup (&req->uri->path->full[pmatch[2].rm_so],
 		    pmatch[2].rm_eo - pmatch[2].rm_so);
   if (metric == NULL) {
+    log_error ("strndup() failed.")
     status = EVHTP_RES_SERVERR;
     goto free_user;
   }
@@ -162,6 +175,7 @@ metric_controller_post (evhtp_request_t *               req,
 
   data = (char *)malloc (evbuffer_get_length(req->buffer_in));
   if (data == NULL) {
+    log_error ("malloc() failed.")
     status = EVHTP_RES_SERVERR;
     goto free_metric;
   }
@@ -171,6 +185,7 @@ metric_controller_post (evhtp_request_t *               req,
 				  evbuffer_get_length(req->buffer_in));
   json_root = json_loadb (data, data_length, 0, &json_error);
   if (json_root == NULL) {
+    log_error ("evbuffer_copyout() failed.")
     status = EVHTP_RES_400;
     goto free_data;
   }
@@ -194,35 +209,48 @@ metric_controller_post (evhtp_request_t *               req,
 
   json_metric_string = json_string (metric);
   if (json_metric_string == NULL) {
+    log_error ("json_string() failed.")
     status = EVHTP_RES_SERVERR;
     goto json_decref;
   }
 
   if (json_object_set (json_root, "metric", json_metric_string) == -1) {
+    log_error ("json_object_set() failed.")
     status = EVHTP_RES_SERVERR;
     goto json_metric_string_decref;
   }
 
   json_dump = json_dumps (json_root, 0);
   if (json_dump == NULL) {
+    log_error ("json_dumps() failed.")
     status = EVHTP_RES_SERVERR;
     goto json_metric_string_decref;
   }
 
-  reply = redisCommand (maytrics_server->redis, "SET %b %b",
+  reply = redisCommand (maytrics->redis, "SET %b %b",
 			&req->uri->path->full[req->uri->path->matched_soff],
 			req->uri->path->matched_eoff - req->uri->path->matched_soff,
 			json_dump, strlen (json_dump));
-  if (reply == NULL) {
+  if (reply == NULL || reply->type == REDIS_REPLY_ERROR) {
+    log_error ("redisCommand() failed: %s", maytrics->redis->errstr);
+    if (reply) {
+      freeReplyObject (reply);
+    }
+
     status = EVHTP_RES_SERVERR;
     goto free_json_dump;
   }
   freeReplyObject (reply);
 
-  reply = redisCommand (maytrics_server->redis, "GET %b",
+  reply = redisCommand (maytrics->redis, "GET %b",
 			user, strlen (user));
 
-  if (reply == NULL) {
+  if (reply == NULL || reply->type == REDIS_REPLY_ERROR) {
+    log_error ("redisCommand() failed: %s", maytrics->redis->errstr);
+    if (reply) {
+      freeReplyObject (reply);
+    }
+
     status = EVHTP_RES_SERVERR;
     goto free_json_dump;
   }
@@ -238,25 +266,34 @@ metric_controller_post (evhtp_request_t *               req,
   freeReplyObject (reply);
 
   if (json_metrics_root == NULL) {
+    log_error ("json_object() or json_loadb() failed.");
+
     status = EVHTP_RES_SERVERR;
     goto free_json_dump;
   }
 
   if (json_object_set (json_metrics_root, metric, json_value) == -1) {
+    log_error ("json_object_set() failed.");
     status = EVHTP_RES_SERVERR;
     goto json_metrics_decref;
   }
 
   json_metrics_dump = json_dumps (json_metrics_root, 0);
   if (json_metrics_dump == NULL) {
+    log_error ("json_dumps() failed.");
     status = EVHTP_RES_SERVERR;
     goto json_metrics_decref;
   }
 
-  reply = redisCommand (maytrics_server->redis, "SET %b %b",
+  reply = redisCommand (maytrics->redis, "SET %b %b",
 			user, strlen (user),
 			json_metrics_dump, strlen (json_metrics_dump));
-  if (reply == NULL) {
+  if (reply == NULL || reply->type == REDIS_REPLY_NIL) {
+    log_error ("redisCommand() failed: %s", maytrics->redis->errstr);
+    if (reply) {
+      freeReplyObject (reply);
+    }
+
     status = EVHTP_RES_SERVERR;
     goto json_metrics_decref;
   }
@@ -299,16 +336,16 @@ metric_controller_post (evhtp_request_t *               req,
 }
 
 void
-metrics_controller (evhtp_request_t * req, void * _maytrics_server)
+metrics_controller (evhtp_request_t * req, void * _maytrics)
 {
-  struct maytrics_server *    maytrics_server =
-    (struct maytrics_server *)_maytrics_server;
+  struct maytrics *    maytrics =
+    (struct maytrics *)_maytrics;
 
   int                         status;
 
   switch (req->method) {
   case htp_method_GET:
-    status = metrics_controller_get (req, maytrics_server);
+    status = metrics_controller_get (req, maytrics);
     break ;
 
   default:
@@ -322,20 +359,20 @@ metrics_controller (evhtp_request_t * req, void * _maytrics_server)
 }
 
 void
-metric_controller (evhtp_request_t * req, void * _maytrics_server)
+metric_controller (evhtp_request_t * req, void * _maytrics)
 {
-  struct maytrics_server *    maytrics_server =
-    (struct maytrics_server *)_maytrics_server;
+  struct maytrics *    maytrics =
+    (struct maytrics *)_maytrics;
 
   int                         status;
 
   switch (req->method) {
   case htp_method_POST:
-    status = metric_controller_post (req, maytrics_server);
+    status = metric_controller_post (req, maytrics);
     break ;
 
   case htp_method_GET:
-    status = metric_controller_get (req, maytrics_server);
+    status = metric_controller_get (req, maytrics);
     break ;
 
   default:
@@ -349,49 +386,63 @@ metric_controller (evhtp_request_t * req, void * _maytrics_server)
 }
 
 int
-init_maytrics_server (struct maytrics_server *  maytrics_server)
+init_maytrics (struct maytrics *  maytrics)
 {
   const char *        port;
+  const char *        log_level_string;
 
-  maytrics_server->host = getenv ("HOST");
-  if (maytrics_server->host == NULL) {
-    maytrics_server->host = "127.0.0.1";
+  maytrics->host = getenv ("HOST");
+  if (maytrics->host == NULL) {
+    maytrics->host = "127.0.0.1";
   }
 
   port = getenv ("PORT");
   if (port == NULL) {
     port = "8081";
   }
+  maytrics->port = atoi (port);
 
-  if (port == NULL) {
-    maytrics_server->port = 8081;
-  }
-  else {
-    maytrics_server->port = atoi (port);
-  }
-
-  maytrics_server->metrics_regex = (regex_t *)malloc (sizeof (regex_t));
-  if (maytrics_server->metrics_regex == NULL) {
+  maytrics->metrics_regex = (regex_t *)malloc (sizeof (regex_t));
+  if (maytrics->metrics_regex == NULL) {
+    log_fatal ("malloc() failed.");  
     goto exit;
   }
-  if (regcomp (maytrics_server->metrics_regex,
+  if (regcomp (maytrics->metrics_regex,
 	       "/api/v1/(.+)/(.+).json",
 	       REG_EXTENDED) == -1) {
-    log_err ("fatal error in regcomp\n");
+    log_fatal ("regcomp() failed");
     goto free_metrics_regex;
+  }
+
+  log_level_string = getenv ("LOG_LEVEL");
+  if (log_level_string == NULL) {
+    return (0);
+  }
+
+  if (!strcmp (log_level_string, "LOG_FATAL")) {
+    log_level = LOG_FATAL;
+  }
+  else if (!strcmp (log_level_string, "LOG_ERROR")) {
+    log_level = LOG_ERROR;
+  }
+  else if (!strcmp (log_level_string, "LOG_INFO")) {
+    log_level = LOG_INFO;
+  }
+  else if (!strcmp (log_level_string, "LOG_DEBUG")) {
+    log_level = LOG_DEBUG;
   }
 
   return (0);
 
  free_metrics_regex:
-  free (maytrics_server->metrics_regex);
+  free (maytrics->metrics_regex);
 
  exit:
   return (-1);
 }
 
 int
-init_redis_client (struct maytrics_server * maytrics_server)
+init_redis_client (struct maytrics * maytrics)
 {
   const char *      host;
   const char *      port;
@@ -406,8 +457,9 @@ init_redis_client (struct maytrics_server * maytrics_server)
     port = "6379";
   }
 
-  maytrics_server->redis = redisConnect (host, atoi (port));
-  if (maytrics_server->redis == NULL) {
+  maytrics->redis = redisConnect (host, atoi (port));
+  if (maytrics->redis == NULL) {
+    log_fatal ("redisConnect(%s, %s) failed.", host, port);
     goto exit;
   }
   return (0);
@@ -424,62 +476,71 @@ main ()
 
   int                         status = 0;
 
-  struct maytrics_server *    maytrics_server;
+  struct maytrics *    maytrics;
 
   evhtp_callback_t *          metrics_controller_cb;
   evhtp_callback_t *          metric_controller_cb;
 
-  maytrics_server = (struct maytrics_server *)malloc (sizeof (struct maytrics_server));
-  if (maytrics_server == NULL) {
+  maytrics = (struct maytrics *)malloc (sizeof (struct maytrics));
+  if (maytrics == NULL) {
+    log_fatal ("malloc() failed.");
     status = 1;
     goto exit;
   }
 
-  if (init_maytrics_server (maytrics_server) == -1) {
+  if (init_maytrics (maytrics) == -1) {
+    log_fatal ("init_maytrics() failed.");
     status = 2;
-    goto free_maytrics_server;
+    goto free_maytrics;
   }
 
   evbase = event_base_new();
   if (evbase == NULL) {
+    log_fatal ("event_base_new() failed.");
     status = 3;
-    goto free_maytrics_server;
+    goto free_maytrics;
   }
 
-  if (init_redis_client (maytrics_server) == -1) {
+  if (init_redis_client (maytrics) == -1) {
+    log_fatal ("init_redis_server() failed.");
     status = 4;
     goto event_base_free;
   }
 
   htp = evhtp_new(evbase, NULL);
   if (htp == NULL) {
+    log_fatal ("evhtp_new() failed.");
     status = 5;
     goto free_redis_client;
   }
 
   metric_controller_cb = evhtp_set_regex_cb (htp, "/api/v1/(.+/.+).json",
 					     metric_controller,
-					     maytrics_server);
+					     maytrics);
   if (metric_controller_cb == NULL) {
+    log_fatal ("evhtp_set_regex_cb() failed.");
     status = 6;
     goto evhtp_free;
   }
 
   metrics_controller_cb = evhtp_set_regex_cb (htp, "/api/v1/(.+).json",
 					      metrics_controller,
-					      maytrics_server);
+					      maytrics);
   if (metrics_controller_cb == NULL) {
+    log_fatal ("evhtp_set_regex_cb() failed.");
     status = 7;
     goto evhtp_free;
   }
 
-  if (evhtp_bind_socket (htp, maytrics_server->host, maytrics_server->port, 1024) != 0) {
+  if (evhtp_bind_socket (htp, maytrics->host, maytrics->port, 1024) != 0) {
+    log_fatal ("evhtp_bind_socket(%s, %d) failed.", maytrics->host, maytrics->port);
     status = 8;
     goto evhtp_free;
   }
-  printf ("Server launched on %s:%d\n", maytrics_server->host, maytrics_server->port);
+  log_info ("Server launched on %s:%d", maytrics->host, maytrics->port);
 
   if (event_base_loop (evbase, 0) == -1) {
+    log_fatal ("event_base_loop() failed.");
     status = 9;
     goto evhtp_unbind_socket;
   }
@@ -491,13 +552,13 @@ main ()
   evhtp_free (htp);
 
  free_redis_client:
-  redisFree (maytrics_server->redis);
+  redisFree (maytrics->redis);
 
  event_base_free:
   event_base_free (evbase);
 
- free_maytrics_server:
-  free (maytrics_server);
+ free_maytrics:
+  free (maytrics);
 
  exit:
   return (status);
