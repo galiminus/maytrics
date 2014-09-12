fdbdf5770bba8d550894322de8e3aa0b9a849429
Victor Goya <goya.victor@gmail.com>
2014-09-12 15:34:06 +0800

    write logs to stdout

--- src/main.c
+++ src/main.c
@@ -23,9 +23,9 @@ struct maytrics_server {
 };
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
-#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
-#define log_debug(M, ...) fprintf(stderr, "[DEBUG] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
-#define log_info(M, ...) fprintf(stderr, "[INFO] " M "\n", ##__VA_ARGS__)
+#define log_err(M, ...) fprintf(stdout, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_debug(M, ...) fprintf(stdout, "[DEBUG] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_info(M, ...) fprintf(stdout, "[INFO] " M "\n", ##__VA_ARGS__)
 
 void
 set_metrics_comment (evhtp_request_t *  req,
